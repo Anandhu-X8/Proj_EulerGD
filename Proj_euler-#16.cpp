6d16f63206f7bd46025641ccb566bde2d44d1bf5
Anandhu-X8 <www.anandhusnair@gmail.com>
2022-12-26 15:07:16 +0530

    #16 solved

--- Proj_euler-#16.cpp
+++ Proj_euler-#16.cpp
@@ -1,3 +1,47 @@
 #include<iostream>
 using namespace std;
-void carry
+
+unsigned long int final (int arrr[]);
+int main()
+{
+   
+    int arry[325]={1};
+
+    for (int  i = 0; i < 100; i++)
+    {
+        int carry=0;
+        for(int j=0;j<325;j++)
+        {
+            int temp=arry[j]*1024+carry;
+            arry[j]=temp%10;
+            carry=temp/10;
+        }
+        
+       
+    }
+    
+    
+
+    unsigned long int sum=final(arry);
+    cout<<sum<<endl;
+    for (int i = 0; i < 325; i++)
+    {
+        cout<<arry[i]<<"  ";
+    }
+    
+    return 0;
+    
+    
+    
+}
+
+unsigned long int final(int arr[])
+{
+    unsigned long int temp=0;
+    for (int i = 0; i < 325; i++)
+    {
+        temp+=arr[i];
+    }
+    return temp;
+}
+
